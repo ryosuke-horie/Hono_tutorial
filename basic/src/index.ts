bea649bb8f656135460191de37a6d753406ef954
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-24 07:56:53 +0900

    Middlewareの確認

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
+import { use } from "hono/jsx";
 
 const app = new Hono();
 
@@ -15,6 +17,20 @@ app.get("/api/hello/:name", (c) => {
     ok: true,
     message: `Hello${name}`,
   });
+});
+
+// basic認証のミドルウェアを使用する
+app.use(
+  "/admin/*",
+  basicAuth({
+    username: "admin",
+    password: "password",
+  })
+);
+
+// basic認証を通過した後の処理
+app.get("/admin", (c) => {
+  return c.text("Welcome to admin page");
 });
 
 export default app;
