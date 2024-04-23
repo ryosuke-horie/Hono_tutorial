0907b39ee325b0e5b75939847c70ea0abeeb64ee
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-04-24 07:51:56 +0900

    テンプレートリテラルに修正

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -1,19 +1,19 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-// リクエストとJson 
+// リクエストとJson
 // cってなに？ → リクエストハンドラの引数。Contextの略。
-app.get('/api/hello/:name', (c) => {
-    const name = c.req.param('name')
-    return c.json({ 
-        ok: true,
-        message: 'Hello' + name
-    })
-})
+app.get("/api/hello/:name", (c) => {
+  const name = c.req.param("name");
+  return c.json({
+    ok: true,
+    message: `Hello${name}`,
+  });
+});
 
-export default app
+export default app;
