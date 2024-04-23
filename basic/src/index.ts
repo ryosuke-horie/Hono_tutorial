a92ff49fcd081a9c86b600e17845b59c085c2b8c
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-23 12:47:36 +0900

    リクエストとJson

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -6,4 +6,14 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+// リクエストとJson 
+// cってなに？ → リクエストハンドラの引数。Contextの略。
+app.get('/api/hello/:name', (c) => {
+    const name = c.req.param('name')
+    return c.json({ 
+        ok: true,
+        message: 'Hello' + name
+    })
+})
+
 export default app
