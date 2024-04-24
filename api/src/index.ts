408d769eeb7001faf769f79be91dbfbd23626376
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-24 12:46:12 +0900

    Appのメモ

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,21 @@
+// Honoが最初のオブジェクトであり、最後まで使われる。
 import { Hono } from 'hono'
 
 const app = new Hono()
+
+// 以下はHono（app）が持つMethod
+// app.HTTP_METHOD([path,]handler|middleware...)
+// app.all([path,]handler|middleware...)
+// app.on(method|method[], path|path[], handler|middleware...)
+// app.use([path,]middleware)
+// app.route(path, [app])
+// app.basePath(path)
+// app.notFound(handler) Not Foundのカスタマイズ可能
+// app.onError(err, handler)  エラーハンドリング
+// app.mount(path, anotherApp) // 他フレームワークをマウントする
+// app.fire()
+// app.fetch(request, env, event) // エントリーポイントとなる。
+// app.request(path, options) // Testに便利。
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
